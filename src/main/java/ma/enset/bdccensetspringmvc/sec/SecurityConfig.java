e1022a8d4a816b823bdb938a0f0e9aa4b1f8908b
Rokaya-mk <mbairikrqia@gmail.com>
2026-06-27 23:50:39 +0100

    securiser l'application

---
+++ src/main/java/ma/enset/bdccensetspringmvc/sec/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ma.enset.bdccensetspringmvc.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password( passwordEncoder().encode("123456") ).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("123456")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("123456")).roles("ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+
+                .build();
+    }
+}
